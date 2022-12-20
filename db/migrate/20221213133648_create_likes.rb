bc2df527566b9b34747eb77c5152b98623668465
FrBarbarossa <makushinsp@mail.ru>
2022-12-20 18:36:23 +0300

    Translation eddited. Migrations fixed. Spec added.

--- db/migrate/20221213133648_create_likes.rb
+++ db/migrate/20221213133648_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.belongs_to :message
-      t.belongs_to :account
+      t.belongs_to :messages
+      t.belongs_to :accounts
       t.timestamps
     end
   end
